990c56070728e32b1e7309a619ad645c1216007f
Cesar <cesar@dmfusion.com>
2019-02-21 17:09:05 -0300

    Fix nombre load_bootstrap

--- includes/scripts.php
+++ includes/scripts.php
@@ -1,7 +1,7 @@
 <?php
 if(!defined('ABSPATH')) exit;
 
-function load_bootstrap(){
+function dm_sliders_load_bootstrap(){
   wp_enqueue_style('bootstrap4', 'https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css');
   wp_enqueue_script( 'boot1','https://code.jquery.com/jquery-3.3.1.min.js', array( 'jquery' ),'',true );
   wp_enqueue_script( 'boot2','https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js', array( 'jquery' ),'',true );
@@ -13,7 +13,7 @@ function load_font_awesome(){
 }
 
 function dm_sliders_frontend_scripts(){
-  load_bootstrap();
+  dm_sliders_load_bootstrap();
 
   wp_enqueue_style('dm_sliders_frontend_css', plugins_url('../assets/css/dm_sliders_style.css', __FILE__), array(), '1.0.1');
   wp_enqueue_script('dm_sliders_frontend_js', plugins_url('../assets/js/dm_sliders_scripts.js', __FILE__), array(), '1.0.1', true);
@@ -28,7 +28,7 @@ function dm_sliders_backend_scripts($hook){
       // Enqueue WordPress media scripts
       wp_enqueue_media();
 
-      load_bootstrap();
+      dm_sliders_load_bootstrap();
       load_font_awesome();
 
       wp_enqueue_style('dm_sliders_backend_css', plugins_url('../assets/css/backend-dm_sliders_style.css', __FILE__), array(), '1.0.0');
